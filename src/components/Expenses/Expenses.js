04031719de201fe6eaaa9d4fd7d6376b957e17a8
ngocha2k <ngocha2k.ln@gmail.com>
2022-03-12 01:09:49 +0700

    Adding new some file to create form and filter by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
 import "./Expenses.css"
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+    const [filteredYear, setFilterYear]= useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilterYear(selectedYear);
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             <ExpenseItem 
             title={props.items[0].title}
             date={props.items[0].date}
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div>
+          <label>Title</label>
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
+        </div>
+        <div>
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+            value={enteredAmount}
+          />
+        </div>
+        <div>
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-30"
+            onChange={dateChangeHandler}
+            value={enteredDate}
+          />
+        </div>
+        <div className="new-expense__actions">
+          <button type="submit">Add Expense</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
